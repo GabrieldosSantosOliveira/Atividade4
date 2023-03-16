83d4e787f77af10219fec39de3bbe6896ebcb479
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-16 13:27:00 -0400

    feat: add styles of app

---
+++ src/styles/ThemeColors.ts
@@ -0,0 +1,8 @@
+export const ThemeColors = {
+  background: '#09090A',
+  white: '#FFFFFF',
+  inputPlaceholder: '#A1A1AA',
+  inputBackground: '#18181B',
+  inputBorder: '#27272A',
+  button: '#16A34A',
+};
